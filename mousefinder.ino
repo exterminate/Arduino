c30bde6c86faf86bb9d24743e0f4d2fe39c84e69
Ian <ian.coates@gmail.com>
2015-11-29 20:35:59 +0000

    added tone()

--- mousefinder.ino
+++ mousefinder.ino
@@ -6,15 +6,19 @@ if it goes past the distance sensor play a sound
 and maybe lights up
 
 */
+#include "pitches.h"
 
 int trigPin = 4;
 int echoPin = 5;
 int led = 2; /* use the built in one */
+int tonePin = 5;
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(led, OUTPUT);
+  pinMode(tonePin, OUTPUT)
+  digitalWrite(led,LOW);
 }
 
 void loop() {
@@ -26,7 +30,12 @@ void loop() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
-  if (distance <= 10) {  // This is where the LED On/Off happens
-    digitalWrite(led4,HIGH);
+  if (distance <= 3) {  // This is where the LED On/Off happens
+    digitalWrite(led,HIGH);
+    for(int i = 0; i <= 20; i++) {
+      tone(tonePin,440,200);
+      tone(tonePin,880,200);
+    }
   }
+  delay(10);
 }
